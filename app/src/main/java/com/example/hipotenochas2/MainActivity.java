fb70411d8b7963846528433bf3c37b59bee222a2
empep <emilio.jose.berlanga.lopez@gmail.com>
2021-11-22 16:35:30 +0100

    Primera aproximación al menú

--- app/src/main/java/com/example/hipotenochas2/MainActivity.java
+++ app/src/main/java/com/example/hipotenochas2/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.hipotenochas2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+ */
 }
